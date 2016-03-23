4b7d48e1d12a366cdfcf73d67228f4e952dc4811
Charlesciezki <charlesciezki@yahoo.com>
2016-03-23 12:24:38 -0500

    Solved problem 2

--- CSharpProblems2/CSharpProblems2/Program.cs
+++ CSharpProblems2/CSharpProblems2/Program.cs
@@ -10,12 +10,17 @@ namespace CSharpProblems2
     {
         static void Main(string[] args)
         {
-            bool isrunning = true;
+           /* bool isrunning = true;
             while (isrunning)
             {
                 UserInputChecker userInputListChecker = new UserInputChecker();
                 userInputListChecker.checkAgainstList();
-            }
+            }*/
+            ShapeMaker shapes = new ShapeMaker(5);
+            shapes.PrintOutForward();
+            shapes.PrintOutUpSideDown();
+            shapes.PrintOutBackWard();
+            Console.ReadLine();
 
         }
     }
---
+++ CSharpProblems2/CSharpProblems2/ShapeMaker.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpProblems2
+{
+    class ShapeMaker
+    {
+        public List<List<string>> ListOfLists = new List<List<string>>();
+        public ShapeMaker(int NumberOfRows)
+        {
+
+            for (int outterIndex = 0; outterIndex < NumberOfRows; outterIndex++)
+            {
+                ListOfLists.Add(new List<string>());
+
+                for (int innerIndex = 0; innerIndex < NumberOfRows; innerIndex++)
+                {
+                    ListOfLists[outterIndex].Add("*");
+                }
+
+            }
+        }
+
+        public void PrintOutForward()
+        {
+            for (int outterIndex = 0; outterIndex < ListOfLists.Count; outterIndex++)
+            {
+                Console.WriteLine();
+                for (int innerIndex = 0; innerIndex < outterIndex + 1; innerIndex++)
+                {
+                    Console.Write(ListOfLists[outterIndex][innerIndex]);
+                }
+            }
+           }
+
+        public void PrintOutUpSideDown()
+        {
+            Console.WriteLine();
+            for (int outterIndex = 4; outterIndex >= 0; outterIndex--)
+            {
+                Console.WriteLine();
+                for (int innerIndex = 0; innerIndex < outterIndex; innerIndex++)
+                {
+                    Console.Write(ListOfLists[outterIndex][innerIndex]);
+                }
+            }
+        }
+
+        public void PrintOutBackWard()
+        {
+            for (int outterIndex = 0; outterIndex < ListOfLists.Count; outterIndex++)
+            {
+                for (int innerIndex = 0; innerIndex < ListOfLists.Count - outterIndex; innerIndex++)
+                {
+                    Console.Write(" ");
+                }
+                for (int innerIndex = 0; innerIndex <= outterIndex  ; innerIndex++)
+                {
+                    Console.Write(ListOfLists[outterIndex][innerIndex]);
+                }
+                Console.WriteLine("");
+            }
+
+        }
+
+    }
+}
+
